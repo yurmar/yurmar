492513a01dc1ccf963102bb2441caff9516c02e8
yurmar <yurmarchuk@gmail.com>
2026-05-25 20:56:11 +0800

    Add favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <title>YurMar</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg">
+    <link rel="icon" type="image/png" href="/images/logo.png" sizes="any">
     @viteReactRefresh
     @vite(['resources/css/app.css', 'resources/js/app.tsx'])
 </head>
